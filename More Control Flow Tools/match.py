966235c2a22e1556472bdd710a9390d78dc3c7d8
ACER\khoav <khoa93076@donga.edu.vn>
2024-05-28 13:35:25 +0700

    More Control Flow Tools

---
+++ More Control Flow Tools/match.py
@@ -0,0 +1,28 @@
+def http_error(status):
+    match status:
+        case 400:
+            return "Bad request"
+        case 401 | 403 | 404:
+            return "Not allowed"
+        case 418:
+            return "I'm a teapot"
+        case _:
+            return "Something's wrong with the internet"
+        
+print(http_error(404))
+
+from enum import Enum
+class Color(Enum):
+    RED = 'red'
+    GREEN = 'green'
+    BLUE = 'blue'
+
+color = Color(input("Enter your choice of 'red', 'blue' or 'green': "))
+
+match color:
+    case Color.RED:
+        print("I see red!")
+    case Color.GREEN:
+        print("Grass is green")
+    case Color.BLUE:
+        print("I'm feeling the blues :(")
