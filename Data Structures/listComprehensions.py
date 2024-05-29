073fffa12f75f817e5254b08a42da83cbabbac91
ACER\khoav <khoa93076@donga.edu.vn>
2024-05-29 08:57:15 +0700

    update list compare

--- Data Structures/listComprehensions.py
+++ Data Structures/listComprehensions.py
@@ -19,3 +19,14 @@ from math import pi
 p = [str(round(pi, i)) for i in range(1,6)]
 print(p)
 
+
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('What is your {0}?  It is {1}.'.format(q, a))
+
+for i in reversed(range(1, 10, 2)):
+    print(i)
+
+compare = (1, 2) < (1, 2, -1)
+print(compare)
